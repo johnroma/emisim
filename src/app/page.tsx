da2eb601057fd143be78d092467a45a11f3bc3c8
johnroma <login@john.ro>
2024-04-19 22:57:22 +0200

    init commit and shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center h-full bg-red-300 p-24">
+      <h1>emisim.com</h1>
+      <Button>Click me</Button>
+    </main>
+  )
+}
